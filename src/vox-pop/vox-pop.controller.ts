8a7658b1986bd072c726abc243679c980fbbddf8
hfht <darth.nater1@gmail.com>
2022-09-21 16:39:50 -0400

    less ambiguous controller func names

--- src/vox-pop/vox-pop.controller.ts
+++ src/vox-pop/vox-pop.controller.ts
@@ -8,14 +8,14 @@ export class VoxPopController {
   constructor(private voxPopService: VoxPopService, private log: LoggerService) {}
 
   @Get()
-  async getFunc(): Promise<string> {
+  async controllerGet(): Promise<string> {
     this.log.write('Vox Pop GET request received');
     return 'this is a get test'
   }
 
   @Post()
   @HttpCode(202) // 202 means accepted/received but not yet acted upon
-  async enqueuePost(@Body() voxPopDTO: voxPopDTO) {
+  async controllerPost(@Body() voxPopDTO: voxPopDTO) {
     if (!!!voxPopDTO) {
       this.log.write(`New Vox Pop Post request is undefined.`);
       throw new HttpException(`payload is of invalid type/must be JSON`, 400)
@@ -25,6 +25,8 @@ export class VoxPopController {
     // TODO: write code necessary for posts to be queued and handled at some interval
 
     // post directly to the blog for now
-    this.voxPopService.createTumblrPost(voxPopDTO)
+    this.voxPopService.createTumblrPost(voxPopDTO);
+
+    return 'Submission received!';
   }
 }
