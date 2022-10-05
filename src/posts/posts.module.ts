3ed9735a2bed92cd1bc7919ba37a33bb2fda7ae7
hfht <darth.nater1@gmail.com>
2022-10-05 14:36:51 -0400

    Remove unused import

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { PostsService } from "./posts.service";
 import { PostsController } from './posts.controller';
-import { PostSchema, Post, PostDocument } from "./schema/post.schema";
+import { PostSchema, Post } from "./schema/post.schema";
 // import mongoose, { Model } from "mongoose";
 
 @Module({
