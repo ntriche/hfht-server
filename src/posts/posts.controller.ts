1e96c750276712fdd945b57189c00d2414f32e20
hfht <darth.nater1@gmail.com>
2022-11-28 17:19:51 -0500

    Correct weird formatting

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -5,7 +5,7 @@ import { Post } from './post.schema';
 @Controller('posts')
 export class PostsController {
 	constructor(private readonly postsService: PostsService) {}
-
+	
 	@Get()
 	async getPosts(): Promise<Post[]> {
 		return this.postsService.find({});
