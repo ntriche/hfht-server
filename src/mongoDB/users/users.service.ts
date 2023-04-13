426edfd8d71388ebeafd5dde06361a063cf3d8a8
buster scruggs <prettyleetemail@protonmail.com>
2023-04-13 15:16:06 -0400

    Add find and findOne methods to users service

--- src/mongoDB/users/users.service.ts
+++ src/mongoDB/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { User, UserDocument } from './user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, FilterQuery } from 'mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+	async find(query: FilterQuery<User>): Promise<User[]> {
+		return this.userModel.find(query);
+	}
+
+	async findOne(query: FilterQuery<User>): Promise<User> {
+		return this.userModel.findOne(query);
+	}
+}
