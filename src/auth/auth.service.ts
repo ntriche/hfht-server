9eefd6bdbf6b4690719843d75331bd3ee9fe7cf6
buster scruggs <prettyleetemail@protonmail.com>
2023-06-03 15:35:58 -0400

    Port over auth changes, add bcrypt, update npm

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { compare, hash } from 'bcrypt';
+import { UsersService } from '../mongoDB/users/users.service';
+
+const saltRounds: number = 10;
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  async authenticateUser(username: string, plaintextPassword: string): Promise<any> {
+    const user = await this.usersService.findOne({'username':username});
+    if (await this.verifyPassword(plaintextPassword, user.password)) {
+      const { password, ...result} = user;
+      return result;
+    }
+    return false;
+  }
+
+  async verifyPassword(plaintextPassword: string, hashedPassword: string): Promise<boolean> {
+    return await compare(plaintextPassword, hashedPassword);
+  }
+
+  async createHash(plaintextPassword: string): Promise<string> {
+    return await hash(plaintextPassword, saltRounds);
+  }
+}
