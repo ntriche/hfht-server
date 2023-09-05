88dcee51a40051e7ae11bb50854744bf2cd785ed
buster scruggs <prettyleetemail@protonmail.com>
2023-09-05 18:25:42 -0400

    export tumblr module

--- src/tumblr/tumblr.module.ts
+++ src/tumblr/tumblr.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TumblrService } from './tumblr.service';
 
 @Module({
-  providers: [TumblrService]
+  providers: [TumblrService],
+  exports: [TumblrService],
 })
 export class TumblrModule {}
