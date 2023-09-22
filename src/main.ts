adf2d4a3c32976a1a27e1e0fa5720dfca3d5fb86
buster scruggs <prettyleetemail@protonmail.com>
2023-09-21 23:55:40 -0400

    Add validation pipeline whitelist options

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,12 @@ declare const module: any;
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
-	app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true
+		})
+	);
 	app.enableCors();
 
 	await app.listen(3000);
