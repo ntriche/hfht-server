ee1454354237a7b731f87222cbb305256bfe9ec9
buster scruggs <prettyleetemail@protonmail.com>
2023-10-12 15:32:57 -0400

    log current env as a debug message on startup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HfhtLoggerService } from './logger/hfht.logger.service';
 
 declare const module: any;
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, {
+		bufferLogs: true,
+	});
+	app.useLogger(app.get(HfhtLoggerService))
+	const log = app.get(HfhtLoggerService);
+
+	const configService = app.get(ConfigService);
+	log.debug(`starting in ${configService.get<string>("NODE_ENV")}`)
 
 	app.useGlobalPipes(
 		new ValidationPipe({
@@ -13,6 +22,7 @@ async function bootstrap() {
 			forbidNonWhitelisted: true
 		})
 	);
+
 	app.enableCors();
 
 	await app.listen(3000);
